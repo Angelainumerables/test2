1ec35243d677936dd529c755445ba0f6d48cf8ea
Angelainumerables <angelainumerables35@gmail.com>
2025-01-21 23:51:38 +0800

    commit changes

--- Financeapp/urls.py
+++ Financeapp/urls.py
@@ -4,11 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.HomePage, name='home'),
     path('transactions/', views.TransactionPage, name='transaction'),
-    path('transactions/create/', views.transaction_create, name='transaction_create'),
+    path('transaction/', views.transaction_list, name='transaction'),
+    path('transaction/create/', views.transaction_create, name='transaction_create'),
     path('transactions/update/<int:pk>/', views.transaction_update, name='transaction_update'),
     path('transactions/delete/<int:pk>/', views.transaction_delete, name='transaction_delete'),
 
     path('budget/', views.budget_list, name='budget_list'),
+    path('budget/', views.budget_list, name='budget'),
     path('budgets/create/', views.budget_create, name='budget_create'),
     path('budgets/update/<int:pk>/', views.budget_update, name='budget_update'),
     path('budgets/delete/<int:pk>/', views.budget_delete, name='budget_delete'),
--- Financeapp/views.py
+++ Financeapp/views.py
@@ -1,17 +1,22 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import *
-from .forms import CategoryForm, TransactionForm
+from .forms import CategoryForm, TransactionForm, BudgetForm
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
 
 def HomePage(request):
     return render(request, 'app/home.html')
 
-
 def TransactionPage(request):
     transactions = Transaction.objects.all()
     return render(request, 'app/transaction_list.html')
+
+def transaction_list(request):
+    transactions = Transaction.objects.all().order_by('-date')
+    return render(request, 'app/transaction_list.html', {'transactions': transactions})
 
 def transaction_create(request):
     if request.method == 'POST':
@@ -20,28 +25,27 @@ def transaction_create(request):
             transaction = form.save(commit=False)
             transaction.user = request.user
             transaction.save()
-            return redirect('transaction_list')
+            return redirect('transaction')
     else:
         form = TransactionForm()
     return render(request, 'app/transaction_form.html', {'form': form})
 
-
 def transaction_update(request, pk):
-    transaction = get_object_or_404(Transaction, pk=pk, user=request.user)
+    transaction = get_object_or_404(Transaction, pk=pk)
     if request.method == 'POST':
         form = TransactionForm(request.POST, instance=transaction)
         if form.is_valid():
             form.save()
-            return redirect('transaction_list')
+            return redirect('transaction')
     else:
         form = TransactionForm(instance=transaction)
     return render(request, 'app/transaction_form.html', {'form': form})
 
 def transaction_delete(request, pk):
-    transaction = get_object_or_404(Transaction, pk=pk, user=request.user)
+    transaction = get_object_or_404(Transaction, pk=pk)
     if request.method == 'POST':
         transaction.delete()
-        return redirect('transaction_list')
+        return redirect('transaction')
     return render(request, 'app/transaction_confirm_delete.html', {'transaction': transaction})
 
 def budget_list(request):
@@ -60,7 +64,7 @@ def budget_create(request):
     return render(request, 'app/budget_form.html', {'form': form})
 
 def budget_update(request, pk):
-    budget = get_object_or_404(Budget, pk=pk, user=request.user)
+    budget = get_object_or_404(Budget, pk=pk)
     if request.method == 'POST':
         form = BudgetForm(request.POST, instance=budget)
         if form.is_valid():
@@ -71,7 +75,7 @@ def budget_update(request, pk):
     return render(request, 'app/budget_form.html', {'form': form})
 
 def budget_delete(request, pk):
-    budget = get_object_or_404(Budget, pk=pk, user=request.user)
+    budget = get_object_or_404(Budget, pk=pk)
     if request.method == 'POST':
         budget.delete()
         return redirect('budget_list')
